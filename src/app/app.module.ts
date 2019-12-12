42e1613ab443c82244c77f5f55eeced628e84eee
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-12 17:43:17 -0600

    created an about-us component and added placeholder text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
