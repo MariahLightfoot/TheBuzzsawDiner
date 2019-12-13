95629ed70aff07d01af21c6aa7ad817dc332c634
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-12 18:02:08 -0600

    created buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
