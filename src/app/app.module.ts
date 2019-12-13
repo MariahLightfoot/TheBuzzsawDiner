404eb678d960e0a9f6e4497c5691c698be96e07e
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-12 20:06:01 -0600

    created an owners section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { OwnersComponent } from './owners/owners.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutUsComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    OwnersComponent
   ],
   imports: [
     BrowserModule
