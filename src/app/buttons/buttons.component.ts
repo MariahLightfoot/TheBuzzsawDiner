ae7878d695b26931bbecee641d219e7b9e4a9b98
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-16 16:07:03 -0600

    still messing with the relief image. can't get it to look nice. also, updated directions button so that you can click anywhere and go to google maps

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -12,4 +12,7 @@ export class ButtonsComponent implements OnInit {
   ngOnInit() {
   }
 
+  goToGoogleMaps() {
+    window.open("https://www.google.com/maps/dir//farmers+kitchen+desoto+mo/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x87d8e22b48c9cacf:0xab2e3288642b7d3b?sa=X&ved=2ahUKEwj3lvLLlLvmAhVIbs0KHXk-BVkQ9RcwDHoECBsQAw");
+  }
 }
